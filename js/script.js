025e5e98575916f2650dcee2a581d9f3e4132ee6
Rinat Raimov <raimovrinat@yandex.ru>
2019-10-28 18:24:43 +0400

    добавил мобильную версию сайта

---
+++ js/script.js
@@ -0,0 +1,53 @@
+var order_btn = document.getElementsByClassName("btn--add_to_cart");
+var modal = document.querySelector(".modal");
+var overlay = document.querySelector(".page-wrapper__overlay");
+var body = document.querySelector("body");
+var modal_close = document.querySelector(".modal__close");
+var hamburger = document.querySelector(".hamburger");
+var menu = document.querySelector(".menu");
+var menuToggle = document.querySelector(".menu__toggle");
+
+for(var i = 0; i < order_btn.length; i++) {
+  order_btn[i].addEventListener("click", function() {
+  modal.style.display = "block";
+  overlay.style.display = "block";
+  body.style.overflow = "hidden";
+  });
+}
+
+modal_close.addEventListener("click", function() {
+  modal.style.display = "none";
+  overlay.style.display = "none";
+  });
+
+
+// обработчик открытия меню
+hamburger.addEventListener("click", function(event) {
+  event.preventDefault();
+  menu.classList.add("menu--opened");
+  overlay.style.display = "block";
+  console.log(menu__links)
+});
+
+// обработчик закрытия меню
+menuToggle.addEventListener("click", function(event) {
+  event.preventDefault();
+  menu.classList.remove("menu--opened");
+  overlay.style.display = "none";
+});
+
+menu.onclick = function(event) {
+  var target = event.target;
+
+  // цикл двигается вверх от target к родителям до table
+  while (target != this) {
+    if (target.tagName == "A") {
+      // нашли элемент, который нас интересует!
+
+      menu.classList.remove("menu--opened");
+      overlay.style.display = "none";
+      return;
+    }
+    target = target.parentNode;
+  }
+}
